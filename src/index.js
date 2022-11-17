0947ec16297024d1dc66d062d7c8b8ba1d7c3ea7
omarhmdea <omarhm@ac.sce.ac.il>
2022-11-17 12:13:17 +0200

    add input for firstName and lastName

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import $ from 'jquery'
-import { createUser } from './rest';
-import { openConnection, sendPlainMessage } from './sockets';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from "jquery";
+import { createUser } from "./rest";
+import { openConnection, sendPlainMessage } from "./sockets";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 $(() => {
-  $(document).on('submit', () => {
-    const user = {
-      email: $('#emailInput').val(),
-      name: $('#userInput').val(),
-      password: $('#passwordInput').val()
-    }
-    createUser(user);
-  })
+	$(document).on("submit", () => {
+		const user = {
+			email: $("#emailInput").val(),
+			firstName: $("#firstNameInput").val(),
+			lastName: $("#lastNameInput").val(),
+			password: $("#passwordInput").val(),
+		};
+		createUser(user);
+	});
 
-  $("#send-btn").on("click", () => {
-    sendPlainMessage("MyUser", $('#message-input').val())
-  })
-
-})
+	$("#send-btn").on("click", () => {
+		sendPlainMessage("MyUser", $("#message-input").val());
+	});
+});
 openConnection();
